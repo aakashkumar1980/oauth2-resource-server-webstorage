270671c54688fdab23081af9cce3f7ec9c0b4072
Aakash Kumar <aakash.kumar1980@yahoo.com>
2023-11-12 00:35:17 +0000

    refactor

--- _test/shell/b.authorize.sh
+++ _test/shell/b.authorize.sh
@@ -3,11 +3,14 @@
 echo "Please login to the Auth0 server to get the authorization code. Manually copy the code and save it for the next step."
 source application.properties
 
-xdg-open "https://${auth0_domain}/authorize?\
-response_type=code&\
-client_id=${client_id}&\
-audience=${audience}&\
-scope=${scope}&\
-code_challenge=${code_challenge}&\
-code_challenge_method=S256&\
-redirect_uri=${callback_uri}" &
+AUTH_URL="$(echo 'https://'${auth0_domain}'/authorize?'\
+'response_type=code&'\
+'client_id='${client_id}'&'\
+'audience='${audience}'&'\
+'scope='${scope}'&'\
+'code_challenge='${code_challenge}'&'\
+'code_challenge_method=S256&'\
+'redirect_uri='${callback_uri})"
+
+echo "AUTH_URL: ${AUTH_URL}"
+xdg-open "${AUTH_URL}" &
