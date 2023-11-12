eb6f58715705045e3c639ba6a54ec4b801d39498
Aakash Kumar <aakash.kumar1980@yahoo.com>
2023-11-12 05:55:31 +0000

    refactored

--- _test/shell/b.authorize.sh
+++ _test/shell/b.authorize.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
+echo "Please select the Authorization Server from the below options:"
+echo "1) Auth0"
+echo "2) AWS Cognito"
+read -p "Enter your choice [1-2]: " choice
+case $choice in
+  1) 
+    echo "loading application-auth0.properties file"
+    source application-auth0.properties
+    ;;
+  2) 
+    echo "loading application-awscognito.properties file"
+    source application-awscognito.properties
+    ;;
+  *)
+    echo "Invalid selection."
+    exit 1
+    ;;
+esac
+# append to application.properties file so that it can be used in the next step
+sed -i "s/^AUTH_SERVER_CHOICE=.*/AUTH_SERVER_CHOICE=$choice/" application.properties
+echo ""
+
+
 echo "Please login to the Auth0 server to get the authorization code. Manually copy the code and save it for the next step."
 source application.properties
-
-AUTH_URL="$(echo 'https://'${auth0_domain}'/authorize?'\
+AUTH_URL="$(echo ${auth_url}\
 'response_type=code&'\
 'client_id='${client_id}'&'\
 'audience='${audience}'&'\
--- _test/shell/c.access_token.sh
+++ _test/shell/c.access_token.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-# NOTE: Please copy the 'code' from the callback URL i.e. http://localhost/oauth2/callback?code=...
-AUTHORIZATION_CODE=""
 source application.properties
+case $AUTH_SERVER_CHOICE in
+  1) 
+    echo "loading application-auth0.properties file"
+    source application-auth0.properties
+    ;;
+  2) 
+    echo "loading application-awscognito.properties file"
+    source application-awscognito.properties
+    ;;
+esac
+echo ""
 
+# NOTE: Please copy the 'code' from the previous step of callback URL i.e. http://localhost/oauth2/callback?code=...
+AUTHORIZATION_CODE=""
 curl --request POST \
-  --url "https://${auth0_domain}/oauth/token" \
+  --url "${token_url}" \
   --header 'content-type: application/x-www-form-urlencoded' \
   --data-urlencode "grant_type=authorization_code" \
   --data-urlencode "client_id=${client_id}" \
@@ -15,7 +26,8 @@ curl --request POST \
   --data-urlencode "redirect_uri=${callback_uri}" \
   --data-urlencode "code_verifier=${code_verifier}" \
   --data-urlencode "code=${AUTHORIZATION_CODE}"
- 
+
+
 
 
 
